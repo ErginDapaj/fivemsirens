16fc9d69bcd28a93445f6ad977344029471334c6
remixor <ergindapaj081@gmail.com>
2022-12-12 22:50:13 +0100

    lol first push

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Snowfall from 'react-snowfall'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>FivemSirens Coming Soon</title>
+        <meta name="description" content="New Fivem Site Coming Soon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <label className={styles.lol}>Coming</label> <a href="https://discord.gg/ewdQPc9G4m">Soon!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Working day and night on this page!
+          <code className={styles.code}>2 Day Streak</code>
+        </p>
+        <div style={{
+          position: 'fixed',
+          width: '100vw',
+          height: '100vh',
+        }}>
+          <Snowfall />
+        </div>
+        <div className={styles.grid}>
+          <a href="https://discord.gg/ewdQPc9G4m" className={styles.card}>
+            <h2>Join Our Discord &rarr;</h2>
+            <p>Join one of the greatest communities!</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        Made with 🖤 by {'Grainger#5445'}
+      </footer>
+    </div>
+  )
+}
